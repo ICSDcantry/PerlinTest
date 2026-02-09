e71307eada15eee06e962b694aac242474ab7317
ICSDcantry <pa4@icsd.k12.ny.us>
2026-02-09 20:59:32 +0000

    idk

---
+++ main.js
@@ -0,0 +1,43 @@
+new p5();
+
+let scale = 5;
+let w = 500;
+let h = 500;
+let seed = 12340;
+let layers = 1;
+let detail = 0.1;
+
+function Color(val){
+    if ((val * 5) % 1 < 0.5) out = 0;
+    else out = 255;
+
+    return [out, out, out, 255];
+}
+
+
+
+
+noiseSeed(seed);
+noiseDetail(layers, detail);
+
+const canvas = document.getElementById("canvas");
+
+canvas.width = w;
+canvas.height = h;
+
+let ctx = canvas.getContext("2d");
+let draw = ctx.createImageData(w, h);
+
+for (var x = 0; x < w; x++) {
+    for (var y = 0; y < h; y++) {
+        let C = Color(noise((x * scale) / w, (y * scale) / h));
+        let i = 4 * (y * w + x);
+
+        draw.data[i] = C[0];
+        draw.data[i + 1] = C[1];
+        draw.data[i + 2] = C[2];
+        draw.data[i + 3] = C[3];
+    }
+}
+
+ctx.putImageData(draw, 0, 0);
