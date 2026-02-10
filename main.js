8dc781cd9eb919cc9a58e5e2a6fb84e3f1328250
ICSDcantry <pa4@icsd.k12.ny.us>
2026-02-10 13:32:42 +0000

    added downloading hopefully

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 new p5();
 
-let scale = 5;
-let w = 500;
-let h = 500;
-let seed = 12340;
-let layers = 1;
-let detail = 0.1;
+
+
+
+let canvas = document.getElementById("canvas");
+let elink = getElementById("exportlink");
+let zval = getElementById("Z");
 
 function Color(val){
     if ((val * 5) % 1 < 0.5) out = 0;
@@ -17,27 +17,44 @@ function Color(val){
 
 
 
-noiseSeed(seed);
-noiseDetail(layers, detail);
+function gen(w, h, scale, layers, detail, seed){
 
-const canvas = document.getElementById("canvas");
+    noiseSeed(seed);
+    noiseDetail(layers, detail);
 
-canvas.width = w;
-canvas.height = h;
+    canvas.width = w;
+    canvas.height = h;
 
-let ctx = canvas.getContext("2d");
-let draw = ctx.createImageData(w, h);
+    let ctx = canvas.getContext("2d");
+    let draw = ctx.createImageData(w, h);
 
-for (var x = 0; x < w; x++) {
-    for (var y = 0; y < h; y++) {
-        let C = Color(noise((x * scale) / w, (y * scale) / h));
-        let i = 4 * (y * w + x);
+    for (var x = 0; x < w; x++) {
+        for (var y = 0; y < h; y++) {
+            let C = Color(noise((x * scale) / w, (y * scale) / h));
+            let i = 4 * (y * w + x);
 
-        draw.data[i] = C[0];
-        draw.data[i + 1] = C[1];
-        draw.data[i + 2] = C[2];
-        draw.data[i + 3] = C[3];
+            draw.data[i] = C[0];
+            draw.data[i + 1] = C[1];
+            draw.data[i + 2] = C[2];
+            draw.data[i + 3] = C[3];
+        }
     }
+
+    ctx.putImageData(draw, 0, 0);
+
 }
 
-ctx.putImageData(draw, 0, 0);
+
+
+let s = 12340;
+let z = 5;
+
+function reloadUI(){
+    zval.innerHTML = z;
+}
+
+gen(500, 500, z, 1, 0.1, s);
+
+function Export(){
+    elink.href = canvas.toDataURL("image/png");
+}
